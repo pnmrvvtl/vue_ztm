c53f29d91b47933bd0c1bac0550812cf852c0eae
pnmrvvtl <pnmrv.vtl@gmail.com>
2023-04-05 12:46:40 +0300

    feat: complete dynamic components task

---
+++ section6_1/dynamic-components/src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Home Page</h1>
+</template>
+
+<script>
+export default {
+  unmounted() {
+    console.log('home unmounted')
+  },
+  activated() {
+    console.log('home activated')
+  },
+  deactivated() {
+    console.log('home deactivated')
+  }
+}
+</script>
