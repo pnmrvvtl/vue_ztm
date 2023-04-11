7ede7d6c089d87528700dfe85f9cf858600811c4
pnmrvvtl <pnmrv.vtl@gmail.com>
2023-04-11 22:34:52 +0300

    feat: complete quiz project from section8_1

---
+++ section8_1/quiz/src/components/Result.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="result">
+    <div class="title">
+      {{ results[resultIndex].title }}
+    </div>
+    <div class="desc">
+      {{ results[resultIndex].desc }}
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  props: ['results', 'totalCorrect'],
+  computed: {
+    resultIndex() {
+      let index = 0;
+      this.results.forEach((item, idx) => {
+        if (item.min <= this.totalCorrect && item.max >= this.totalCorrect) {
+          index = idx;
+        }
+      })
+      return index;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
