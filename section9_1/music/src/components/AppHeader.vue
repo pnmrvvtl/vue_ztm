b777584e66f787fc93ca0b35eaa2559286bc7cd9
pnmrvvtl <pnmrv.vtl@gmail.com>
2023-04-26 02:19:46 +0300

    feat: complete routing for music app

--- section9_1/music/src/components/AppHeader.vue
+++ section9_1/music/src/components/AppHeader.vue
@@ -2,7 +2,12 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <a class="text-white font-bold uppercase text-2xl mr-4" href="#">Music</a>
+      <router-link
+        class="text-white font-bold uppercase text-2xl mr-4"
+        :to="{ name: 'home' }"
+        exact-active-class="no-active"
+        >Music</router-link
+      >
 
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
@@ -15,14 +20,19 @@
           </li>
           <template v-else>
             <li>
-              <a class="px-2 text-white" href="#">Manage</a>
-            </li>
-            <li>
-              <a class="px-2 text-white" href="#" @click.prevent="userStore.signOut"
-                >Logout</a
+              <router-link class="px-2 text-white" :to="{ name: 'manage' }"
+                >Manage</router-link
               >
             </li>
+            <li>
+              <a class="px-2 text-white" href="#" @click.prevent="signOut">Logout</a>
+            </li>
           </template>
+          <li>
+            <router-link class="px-2 text-white" :to="{ name: 'about' }"
+              >About</router-link
+            >
+          </li>
         </ul>
       </div>
     </nav>
@@ -43,6 +53,15 @@ export default {
   methods: {
     toggleAuthModal() {
       this.isOpen = !this.isOpen;
+    },
+    signOut() {
+      this.userStore.signOut();
+
+      if (this.$route.meta.requiresAuth) {
+        this.$router.push({
+          name: 'home',
+        });
+      }
     },
   },
 };
--- section9_1/music/src/router/index.js
+++ section9_1/music/src/router/index.js
@@ -1,23 +1,62 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '@/views/HomeView.vue';
+import AboutView from '@/views/AboutView.vue';
+import ManageView from '@/views/ManageView.vue';
+import useUserStore from '@/stores/user';
+
+const routes = [
+  {
+    name: 'home',
+    path: '/',
+    component: HomeView,
+  },
+  {
+    name: 'about',
+    path: '/about',
+    component: AboutView,
+  },
+  {
+    name: 'manage',
+    // alias: '/manage',
+    path: '/manage-music',
+    component: ManageView,
+    beforeEnter: (to, from, next) => {
+      console.log('Manage route guard.');
+      next();
+    },
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    path: '/manage',
+    redirect: { name: 'manage' },
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'home' },
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+  routes,
+  linkExactActiveClass: 'text-yellow-500 underline',
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  console.log('Global guard.');
+  if (!to.meta.requiresAuth) {
+    next();
+    return;
+  }
+  if (useUserStore().userLoggedIn) {
+    next();
+  } else {
+    next({ name: 'home' });
+  }
+
+  next();
+});
+
+export default router;
